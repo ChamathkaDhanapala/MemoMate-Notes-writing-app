6bc208ab974621b40eaf15aa6a7faf23622e74a8
ChamathkaDhanapala <139616499+ChamathkaDhanapala@users.noreply.github.com>
2025-03-29 19:54:28 +0530

    Create Login page and put some styles

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import { Link } from "react-router-dom";
+import "../Login/Login.css"
 const Login = () => {
     return(
-        <div>Login</div>
+        <>
+        <Navbar/>
+
+        <div className="login-container"> 
+            <div className="login-box">
+                <form onSubmit={()=>{}}>
+                    <h4>Login</h4>
+                    <input type="text" placeholder="Email" className="input-box"/>
+                    <button type="submit" className="btn-primary">Login</button>
+                    <p>Not Registered yet?{""}
+                        <Link to="/signup">Create an Account</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
+        </>
     )
-}
+    
+};
 export default Login
