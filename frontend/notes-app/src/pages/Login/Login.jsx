88edefd6990e8bcf7708084ca0ead0157ef54bc5
ChamathkaDhanapala <139616499+ChamathkaDhanapala@users.noreply.github.com>
2025-03-31 13:07:36 +0530

    Add email and password validation logic

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -3,39 +3,58 @@ import Navbar from "../../components/Navbar/Navbar";
 import { Link, useActionData } from "react-router-dom";
 import "../Login/Login.css"
 import PasswordInput from "../../components/Input/PasswordInput"
+import { validateEmail } from "../../utils/helper";
 
 const Login = () => {
 
-    const [email,setEmai] = useState("");
-    const [password,setPassword] = useState("");
-    const [error,setError] = useState(null);
+    const [email, setEmai] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
 
-    const handleLogin = async(e)=>{
+    const handleLogin = async (e) => {
         e.preventDefault();
+
+        if (!validateEmail(email)) {
+            setError("Please enter a valid email address.");
+            return;
+        }
+
+        if(!password){
+            setError("Please enter the password");
+            return;
+        }
+
+        setError("")
     };
-    return(
+    return (
         <>
-        <Navbar/>
+            <Navbar />
 
-        <div className="login-container"> 
-            <div className="login-box">
-                <form onSubmit={handleLogin}>
-                    <h4>Login</h4>
-                    <input type="text" placeholder="Email" className="input-box"
-                    value={email} 
-                    onChange={(e)=>setEmai(e.target.value)}/>
+            <div className="login-container">
+                <div className="login-box">
+                    <form onSubmit={handleLogin}>
+                        <h4>Login</h4>
+                        <input type="text"
+                            placeholder="Email"
+                            className="input-box"
+                            value={email}
+                            onChange={(e) => setEmai(e.target.value)} />
 
-                    <PasswordInput value={password}
-                    onChange={(e)=>setPassword(e.target.value)}/>
-                    <button type="submit" className="btn-primary">Login</button>
-                    <p>Not Registered yet?{""}
-                        <Link to="/signup">Create an Account</Link>
-                    </p>
-                </form>
+                        <PasswordInput
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)} />
+
+                            {error && <p className="err-email">{error}</p>}
+
+                        <button type="submit" className="btn-primary">Login</button>
+                        <p>Not Registered yet?{""}
+                            <Link to="/signup">Create an Account</Link>
+                        </p>
+                    </form>
+                </div>
             </div>
-        </div>
         </>
     )
-    
+
 };
 export default Login
