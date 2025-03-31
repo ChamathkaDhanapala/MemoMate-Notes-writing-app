767cd9666b73b6ba6771e49f6b8aa2c2506c8924
ChamathkaDhanapala <139616499+ChamathkaDhanapala@users.noreply.github.com>
2025-03-31 09:18:11 +0530

    Put state setter function for email and password

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import { Link } from "react-router-dom";
+import { Link, useActionData } from "react-router-dom";
 import "../Login/Login.css"
 import PasswordInput from "../../components/Input/PasswordInput"
 
 const Login = () => {
+
+    const [email,setEmai] = useState("");
+    const [password,setPassword] = useState("");
+    const [error,setError] = useState(null);
+
+    const handleLogin = async(e)=>{
+        e.preventDefault();
+    };
     return(
         <>
         <Navbar/>
 
         <div className="login-container"> 
             <div className="login-box">
-                <form onSubmit={()=>{}}>
+                <form onSubmit={handleLogin}>
                     <h4>Login</h4>
-                    <input type="text" placeholder="Email" className="input-box"/>
-                    <PasswordInput/>
+                    <input type="text" placeholder="Email" className="input-box"
+                    value={email} 
+                    onChange={(e)=>setEmai(e.target.value)}/>
+
+                    <PasswordInput value={password}
+                    onChange={(e)=>setPassword(e.target.value)}/>
                     <button type="submit" className="btn-primary">Login</button>
                     <p>Not Registered yet?{""}
                         <Link to="/signup">Create an Account</Link>
