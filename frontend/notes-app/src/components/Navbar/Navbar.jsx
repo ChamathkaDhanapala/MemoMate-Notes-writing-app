3fb4bd5238130a46f200757544751be757476303
ChamathkaDhanapala <139616499+ChamathkaDhanapala@users.noreply.github.com>
2025-05-09 16:02:09 +0530

    Input clear functionality in Navbar

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,29 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Navbar/Navbar.css";
 import ProfileInfo from "../Cards/ProfileInfo";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 
 const Navbar = () => {
-  const navigate = useNavigate();
+    const [searchQuery, setSearchQuery] = useState("");
+    const navigate = useNavigate();
 
-  const onLogout = () => {
-    navigate("/login");
-  };
+    const onLogout = () => {
+        navigate("/login");
+    };
 
-  return (
-    <div className="navbar-container">
-      <h2 className="navbar-title">Notes</h2>
+    const handleSearch = () => { };
 
-      <div className="searchbar-wrapper">
-        <SearchBar />
-      </div>
+    const onClearSearch = () => {
+        setSearchQuery("");
+    };
 
-      <div className="profile-wrapper">
-        <ProfileInfo onLogout={onLogout} />
-      </div>
-    </div>
-  );
+    return (
+        <div className="navbar-container">
+            <h2 className="navbar-title">Notes</h2>
+
+            <div className="searchbar-wrapper">
+                <SearchBar
+                    value={searchQuery}
+                    onChange={({ target }) => {
+                        setSearchQuery(target.value);
+                    }}
+                    handleSearch={handleSearch}
+                    onClearSearch={onClearSearch}
+                />
+            </div>
+
+            <div className="profile-wrapper">
+                <ProfileInfo onLogout={onLogout} />
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
