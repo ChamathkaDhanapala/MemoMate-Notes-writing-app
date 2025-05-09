8d9756d2d52565f421d1dfb3a8227d790c6ada79
ChamathkaDhanapala <139616499+ChamathkaDhanapala@users.noreply.github.com>
2025-05-09 15:39:40 +0530

    Add SearchBar component with input, search, and clear functionality

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../Navbar/Navbar.css";
 import ProfileInfo from "../Cards/ProfileInfo";
 import { useNavigate } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
 
 const Navbar = () => {
     const navigate = useNavigate;
@@ -14,6 +15,7 @@ const Navbar = () => {
         <div className="navbar-container">
             <h2 className="navbar-title">Notes</h2>
 
+            <SearchBar/>
             <ProfileInfo onLogout={onLogout}/>
         </div>
     );
--- frontend/notes-app/src/components/SearchBar/SearchBar.jsx
+++ frontend/notes-app/src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import {FaMagnifyingGlass} from "react-icons/fa6";
+import {IoMdClose} from "react-icons/io";
 
-const SearchBar = () => {
+const SearchBar = ({value, onChange, handleSearch, onClearSearch}) => {
     return(
-        <div>SearchBar</div>
+        <div>
+            <input
+            type="text"
+            placeholder="Search Notes"
+            className=""
+            value={value}
+            onChange={onChange}
+            />
+
+            {value &&(
+                <IoMdClose className="" onClick={onClearSearch}
+                />
+    )}
+
+            <FaMagnifyingGlass className="" onClick={handleSearch}/>
+        </div>
     )
 }
 
